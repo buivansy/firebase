2414e8972b67eb41fa2119ade5af0e320822ff63
unknown <sy.buivan@comartek.vn>
2020-08-27 11:37:22 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Login from './containers/login';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Login);
